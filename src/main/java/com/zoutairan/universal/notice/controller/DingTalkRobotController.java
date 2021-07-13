25061cbf19c125a66636d3f0d4b60f942362ef99
zoutairan <e_jjk@163.com>
2021-07-13 15:26:05 +0800

    refactor: 参数配置化

--- src/main/java/com/zoutairan/universal/notice/controller/DingTalkRobotController.java
+++ src/main/java/com/zoutairan/universal/notice/controller/DingTalkRobotController.java
@@ -1,5 +1,6 @@
 package com.zoutairan.universal.notice.controller;
 
+import cn.hutool.core.text.CharSequenceUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zoutairan.universal.notice.dao.DingTalkUserDao;
 import com.zoutairan.universal.notice.entity.DingTalkUserEntity;
@@ -7,6 +8,7 @@ import com.zoutairan.universal.notice.pojo.Message;
 import com.zoutairan.universal.notice.pojo.Message.At;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,15 +40,23 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @RestController
 @RequestMapping("robot")
 public class DingTalkRobotController {
+    private static final String URL_FORMAT_TEXT = "{}?access_token={}&timestamp={}&sign={}";
+
     @Autowired
     private DingTalkUserDao dingTalkUserDao;
 
+    @Value("${ding-talk.robot.send-message.url}")
+    private String url;
+
+    @Value("${ding-talk.robot.send-message.access-token}")
+    private String accessToken;
+
+    @Value("${ding-talk.robot.send-message.secret}")
+    private String secret;
+
     @PostMapping("send")
     public String send(@RequestParam("ownerName") String ownerName, @RequestBody Message message) throws NoSuchAlgorithmException, InvalidKeyException {
-        var url = "https://oapi.dingtalk" +
-                ".com/robot/send?access_token=644bba67c37b732c35d01809c5c55e3e3e26a6c9bfa373b5c16f7b56ed88e654";
-        var secret = "SECf58d5116f64f5cd0e2d2200f70b4cb772b90e11a49706bdfa56e7c5ee9465c2b";
-        url = buildUrl(url, secret);
+        url = buildUrl(url, accessToken, secret);
         var restTemplate = new RestTemplate();
         QueryWrapper<DingTalkUserEntity> queryWrapper = new QueryWrapper<DingTalkUserEntity>().eq("name", ownerName);
         var dingTalkUserEntity = dingTalkUserDao.selectOne(queryWrapper);
@@ -56,14 +66,14 @@ public class DingTalkRobotController {
         return restTemplate.postForObject(url, request, String.class);
     }
 
-    private String buildUrl(String url, String secret) throws NoSuchAlgorithmException, InvalidKeyException {
+    private String buildUrl(String url, String accessToken, String secret) throws NoSuchAlgorithmException,
+            InvalidKeyException {
         long timestamp = System.currentTimeMillis();
         var stringToSign = timestamp + "\n" + secret;
         Mac mac = Mac.getInstance("HmacSHA256");
         mac.init(new SecretKeySpec(secret.getBytes(UTF_8), "HmacSHA256"));
         byte[] signData = mac.doFinal(stringToSign.getBytes(UTF_8));
         String sign = URLEncoder.encode(new String(Base64.encodeBase64(signData)), UTF_8);
-        url = url + "&timestamp=" + timestamp + "&sign=" + sign;
-        return url;
+        return CharSequenceUtil.format(URL_FORMAT_TEXT, url, accessToken, timestamp, sign);
     }
 }
